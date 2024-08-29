e0a6fcc279998fe775a53eca7dd73b9c679cf9f5
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-08-29 23:57:48 +0700

    create HowWorks comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/banner/Banner";
 import SearchBar from "@/components/search-bar/SearchBar";
+import HowWorks from "../components/how-work/HowWorks";
 
 const Home = () => {
   return (
     <main>
       <Banner />
       <SearchBar />
+      <HowWorks />
     </main>
   );
 };
---
+++ components/how-work/HowWorks.tsx
@@ -0,0 +1,54 @@
+const HowWorks = () => {
+  return (
+    <section className="mt-40 text-center">
+      <h2 className="text-2xl font-bold">How Rabiot Car Rental Works</h2>
+      <p className="mt-3">
+        The proper business solution for your developing business
+      </p>
+      <div className="flex gap-6 mt-[60px] mx-16">
+        <div className="flex items-center">
+          <p className="text-[50px]">01</p>
+          <div className="ml-3 text-left">
+            <h3 className=" text-lg font-bold">Select Car Condition</h3>
+            <p className="mt-3 text-sm">
+              Proper Business solution for your developing business strategies
+              and corporation
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <p className="text-[50px]">01</p>
+          <div className="ml-3 text-left">
+            <h3 className=" text-lg font-bold">Select Car Condition</h3>
+            <p className="mt-3 text-sm">
+              Proper Business solution for your developing business strategies
+              and corporation
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <p className="text-[50px]">01</p>
+          <div className="ml-3 text-left">
+            <h3 className=" text-lg font-bold">Select Car Condition</h3>
+            <p className="mt-3 text-sm">
+              Proper Business solution for your developing business strategies
+              and corporation
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <p className="text-[50px]">01</p>
+          <div className="ml-3 text-left">
+            <h3 className=" text-lg font-bold">Select Car Condition</h3>
+            <p className="mt-3 text-sm">
+              Proper Business solution for your developing business strategies
+              and corporation
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowWorks;
