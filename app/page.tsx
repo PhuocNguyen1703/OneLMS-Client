fa7e8c2e089ab60f36e76373cc3f38f05d9d3ffe
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-08-29 23:08:33 +0700

    create search-bar comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/banner/Banner";
+import SearchBar from "@/components/search-bar/SearchBar";
 
 const Home = () => {
   return (
     <main>
       <Banner />
+      <SearchBar />
     </main>
   );
 };
