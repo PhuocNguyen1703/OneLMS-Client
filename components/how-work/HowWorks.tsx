7500ff63a878f97e88c2bf6536cb6771340cd404
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-09-23 14:47:20 +0700

    update ui HowWorks compt

--- components/how-work/HowWorks.tsx
+++ components/how-work/HowWorks.tsx
@@ -1,51 +1,56 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Box, CalendarSearch, FilePenLine, Wrench } from "lucide-react";
+import Image from "next/image";
+
 const HowWorks = () => {
+  const stepList = [
+    {
+      icon: <CalendarSearch />,
+      title: "Reservation and Booking",
+      desc: "The customer selects a vehicle and rental dates, either online or at a rental office. They confirm availability, review the price, and complete the reservation by providing their personal details and payment information.",
+    },
+    {
+      icon: <Wrench />,
+      title: "Vehicle Preparation",
+      desc: "The rental company ensures the car is ready by performing maintenance checks, cleaning the vehicle, and documenting any existing damages in a pre-rental inspection report.",
+    },
+    {
+      icon: <FilePenLine />,
+      title: "Pickup and Agreement",
+      desc: "The customer arrives, presents a valid driver’s license, and signs a rental agreement outlining insurance options, mileage limits, and return conditions. After a final inspection with the rental agent, the vehicle is handed over to the customer.",
+    },
+    {
+      icon: <Box />,
+      title: "Return and Final Inspection",
+      desc: "The customer returns the vehicle on the agreed date. The company performs a final inspection to check for any new damages or extra mileage. Final charges are processed, and any security deposits are refunded, concluding the rental.",
+    },
+  ];
   return (
     <section className="mt-40 text-center">
-      <h2 className="text-2xl font-bold">How Rabiot Car Rental Works</h2>
-      <p className="mt-3">
+      <h2 className="text-4xl font-bold">How Rainbow Car Rental Works</h2>
+      <p className="mt-2">
         The proper business solution for your developing business
       </p>
-      <div className="flex gap-6 mt-[60px] mx-16">
-        <div className="flex items-center">
-          <p className="text-[50px]">01</p>
-          <div className="ml-3 text-left">
-            <h3 className=" text-lg font-bold">Select Car Condition</h3>
-            <p className="mt-3 text-sm">
-              Proper Business solution for your developing business strategies
-              and corporation
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <p className="text-[50px]">01</p>
-          <div className="ml-3 text-left">
-            <h3 className=" text-lg font-bold">Select Car Condition</h3>
-            <p className="mt-3 text-sm">
-              Proper Business solution for your developing business strategies
-              and corporation
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <p className="text-[50px]">01</p>
-          <div className="ml-3 text-left">
-            <h3 className=" text-lg font-bold">Select Car Condition</h3>
-            <p className="mt-3 text-sm">
-              Proper Business solution for your developing business strategies
-              and corporation
-            </p>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <p className="text-[50px]">01</p>
-          <div className="ml-3 text-left">
-            <h3 className=" text-lg font-bold">Select Car Condition</h3>
-            <p className="mt-3 text-sm">
-              Proper Business solution for your developing business strategies
-              and corporation
-            </p>
-          </div>
-        </div>
+      <div className="flex justify-center gap-6 mt-[60px] ">
+        {stepList.map((item) => (
+          <Card key={item.title} className="w-[350px]">
+            <CardHeader>
+              <div className="w-fit p-2 border rounded-full">{item.icon}</div>
+              <CardTitle className="!mt-4">{item.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <CardDescription className="text-base text-left">
+                {item.desc}
+              </CardDescription>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </section>
   );
