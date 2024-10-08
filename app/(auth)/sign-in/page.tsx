689def4f1d9b859e732e9459d0af3d2fda32d50a
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-10-09 02:57:11 +0700

    create ResetPasswordForm

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,22 @@
+import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 import SignInForm from "@/components/auth/SignInForm";
 
 const SignIn = () => {
   return (
-    <section className="w-[470px] p-10 border">
-      <h1 className="text-4xl font-bold text-center">Welcome</h1>
-      <SignInForm />
-    </section>
+    <>
+      {/* <section className="w-[470px] p-10 border">
+        <h1 className="text-4xl font-bold text-center">Welcome</h1>
+        <SignInForm />
+      </section> */}
+      <section className="w-[470px] p-10 border">
+        <h1 className="text-4xl font-bold text-center">Password Recovery</h1>
+        <p className="m-2 text-gray-400">
+          Enter your registered email address. We&apos;ll send you a code to
+          reset your password.
+        </p>
+        <ResetPasswordForm />
+      </section>
+    </>
   );
 };
 
---
+++ components/auth/ResetPasswordForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  email: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+const ResetPasswordForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form className="mt-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-base">Email</FormLabel>
+              <FormControl>
+                <Input
+                  className="h-12 rounded-[8px] text-base"
+                  placeholder="Enter your email"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full h-12 mt-10 text-base" type="submit">
+          Send OTP
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default ResetPasswordForm;
