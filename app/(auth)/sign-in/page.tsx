80b91d5d718660039c5d0d99b700da363b4f1cf2
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-10-09 04:02:53 +0700

    create OTP form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import OtpForm from "@/components/auth/OtpForm";
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 import SignInForm from "@/components/auth/SignInForm";
 
@@ -8,13 +9,20 @@ const SignIn = () => {
         <h1 className="text-4xl font-bold text-center">Welcome</h1>
         <SignInForm />
       </section> */}
-      <section className="w-[470px] p-10 border">
+      {/* <section className="w-[470px] p-10 border">
         <h1 className="text-4xl font-bold text-center">Password Recovery</h1>
         <p className="m-2 text-gray-400">
           Enter your registered email address. We&apos;ll send you a code to
           reset your password.
         </p>
         <ResetPasswordForm />
+      </section> */}
+      <section className="w-[470px] p-10 border">
+        <h1 className="text-4xl font-bold text-center">Email Verification</h1>
+        <p className="m-2 text-gray-400">
+          We have share a code of your registered email address ***@gmail.com
+        </p>
+        <OtpForm />
       </section>
     </>
   );
---
+++ components/auth/OtpForm.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+const FormSchema = z.object({
+  pin: z.string().min(6, {
+    message: "Your one-time password must be 6 characters.",
+  }),
+});
+
+const OtpForm = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      pin: "",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof FormSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col mt-5 items-center"
+      >
+        <FormField
+          control={form.control}
+          name="pin"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <InputOTP maxLength={6} {...field}>
+                  <InputOTPGroup className="h-[60px] gap-4">
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={0}
+                    />
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={1}
+                    />
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={2}
+                    />
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={3}
+                    />
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={4}
+                    />
+                    <InputOTPSlot
+                      className="h-full text-2xl border-l rounded-md"
+                      index={5}
+                    />
+                  </InputOTPGroup>
+                </InputOTP>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button className="w-2/3 h-12 mt-5 text-base" type="submit">
+          Verify Account
+        </Button>
+        <span className="block mt-5 text-center">
+          Don&apos;t receive code? <strong>Resend</strong>
+        </span>
+      </form>
+    </Form>
+  );
+};
+
+export default OtpForm;
