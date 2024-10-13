115ebe0a5f4338051b6bc01fb174b8257c6bc512
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-10-14 05:04:52 +0700

    fix UI SignIn modal

--- components/auth/sign-in/SignIn.tsx
+++ components/auth/sign-in/SignIn.tsx
@@ -1,7 +1,7 @@
 import SignInForm from "@/components/auth/sign-in/SignInForm";
 import { Modal } from "@/components/Modal";
 import { Button } from "@/components/ui/button";
-import { CircleX } from "lucide-react";
+import { CircleX, Facebook } from "lucide-react";
 
 const SignIn = () => {
   return (
@@ -13,8 +13,29 @@ const SignIn = () => {
         >
           <CircleX />
         </Button>
-        <h1 className="text-4xl font-bold text-center">Welcome</h1>
+        <h1 className="text-4xl font-bold text-center">Sign in to Rainbow</h1>
+        <p className="text-base text-center">
+          Welcome back! Please sign in to continue.
+        </p>
+        <div className="flex items- gap-8 mt-5">
+          <Button variant="outline" className="w-full gap-1 h-12 text-base">
+            <Facebook />
+            <p>Google</p>
+          </Button>
+          <Button variant="outline" className="w-full gap-1 h-12 text-base">
+            <Facebook />
+            <p>Facebook</p>
+          </Button>
+        </div>
+        <div className="relative mt-5 text-center after:absolute after:top-1/2 after:left-0 after:h-[1px] after:w-full after:bg-muted-foreground">
+          <span className="relative px-2 bg-white z-10 cursor-default select-none">
+            or
+          </span>
+        </div>
         <SignInForm />
+        <p className="block mt-5 text-center">
+          Don&apos;t have an account? <strong>Sign up</strong>
+        </p>
       </section>
     </Modal>
   );
