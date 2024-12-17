0e3d7585d1616694cafa7702d7d0bf041a9ce4bc
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-12-18 02:54:10 +0700

    create middleware handle path

---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const publicPaths = ["/sign-in"];
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isPublicPath = publicPaths.some((path) => pathname.startsWith(path));
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  if (!isPublicPath && !accessToken) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
