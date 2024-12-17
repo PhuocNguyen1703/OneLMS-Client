f14921c7571091c77bbd43d7304f47ebbbd6581b
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2024-12-17 23:10:34 +0700

    remove next auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout({
     <section className="flex items-center justify-center h-screen">
       <div className="flex p-5 border rounded-lg bg-primary-foreground">
         <div className="rounded-lg">
-          <ImageSlide />
+          {/* <ImageSlide /> */}
         </div>
         <div className="flex flex-col">
           <div className="flex items-center justify-center gap-2 mt-6">
